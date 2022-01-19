a029f0085bf911f95797f60ac1a23293faba8f71
Ivan <ivan129522@gmail.com>
2022-01-20 00:00:20 +0200

    Automate user input and add validation

--- js/main.js
+++ js/main.js
@@ -8,12 +8,27 @@ const personalMovieDB = {
 	private: false
 };
 
-const firstFilmTitle = prompt('One of the last movies you saw?', '');
-const firstFilmRate = prompt('How much would you estimate it?', '');
-const secondFilmTitle  = prompt('One of the last movies you saw?', '');
-const secondFilmRate = prompt('How much would you estimate it?', '');
+// const firstFilmTitle = prompt('One of the last movies you saw?', '');
+// const firstFilmRate = prompt('How much would you estimate it?', '');
+// const secondFilmTitle = prompt('One of the last movies you saw?', '');
+// const secondFilmRate = prompt('How much would you estimate it?', '');
 
-personalMovieDB.movies[firstFilmTitle] = firstFilmRate;
-personalMovieDB.movies[secondFilmTitle] = secondFilmRate;
+// automate user input use while
+let i = 2;
+let filmTitle,
+		filmRate;
+
+while (i) {
+	filmTitle = prompt('One of the last movies you saw?', '');
+	filmRate = +prompt('How much would you estimate it?', '');
+
+	if (filmTitle === '' || filmRate === 0 || filmTitle.length > 50 ||
+			filmTitle === null || filmRate === null) {
+		i = 2;
+	} else {
+		personalMovieDB.movies[filmTitle] = filmRate;
+	}
+	i--;
+}
 
 console.table(personalMovieDB);
