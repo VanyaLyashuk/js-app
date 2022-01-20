283d4da8638379d7db56b37aa80c3234a059e0aa
Ivan <ivan129522@gmail.com>
2022-01-20 08:11:17 +0200

    Replace (do while) cycle to (for)

--- js/main.js
+++ js/main.js
@@ -8,28 +8,21 @@ const personalMovieDB = {
 	private: false
 };
 
-// const firstFilmTitle = prompt('One of the last movies you saw?', '');
-// const firstFilmRate = prompt('How much would you estimate it?', '');
-// const secondFilmTitle = prompt('One of the last movies you saw?', '');
-// const secondFilmRate = prompt('How much would you estimate it?', '');
-
 // automate user input use while
-let i = 2;
 let filmTitle,
 		filmRate;
 
-do {
+for (let i = 1; i <= 2; i++) {
 	filmTitle = prompt('One of the last movies you saw?', '');
 	filmRate = +prompt('How much would you estimate it?', '');
 
 	if (filmTitle === '' || filmRate === 0 || filmTitle.length > 50 ||
 			filmTitle === null || filmRate === null) {
-		i = 2;
+		i = 1;
 	} else {
 		personalMovieDB.movies[filmTitle] = filmRate;
 	}
-	i--;
-} while (i);
+}
 
 if (personalMovieDB.count < 10) {
 	alert('Seen quite a few movies.');
